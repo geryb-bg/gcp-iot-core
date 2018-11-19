e5991e3170f1241f492b8b0dd4cfa7ebae655be8
Gergana Young <gergana@bbd.co.za>
2018-11-19 13:54:39 +0200

    updating order

---
+++ 3. combined/args.js
@@ -0,0 +1,7 @@
+module.exports = {
+    projectId: 'iot-cloud-stuff',
+    cloudRegion: 'europe-west1',
+    registryId: 'iot-registry',
+    deviceId: 'r2-d2-pi',
+    privateKeyFile: '../rsa_r2_d2_private.pem',
+}
